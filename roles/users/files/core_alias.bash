3266ed9e7a7a8f1d515a43eae835be432450b542
Javier Rojas <fjrojas@aubay.es>
2022-07-14 13:51:31 +0200

    initial commit

---
+++ roles/users/files/core_alias.bash
@@ -0,0 +1,7 @@
+alias la='ls -lAh'
+alias l='ls -lCF'
+alias ll='ls -alF'
+alias ls='ls --color=auto'
+alias sl="ls"
+#alias rabbitmqctl="docker exec -it $(docker ps -f name=aubay-sam-stack_rabbitmq --quiet) rabbitmqctl"
+#alias sam_api_log_follow="docker logs --follow $(docker ps -f name=aubay-sam-stack_aubay-sam-api --quiet)"
---
+++ roles/users/files/sam_alias.bash
@@ -0,0 +1,6 @@
+alias check-service="/etc/sam/bin/check-services"
+alias stop-service="/etc/sam/bin/stop-service"
+alias start-service="/etc/sam/bin/start-service"
+alias backup="/etc/sam/bin/postgis-backup"
+alias restore="/etc/sam/bin/postgis-restore"
+alias logs="/etc/sam/bin/logs"
