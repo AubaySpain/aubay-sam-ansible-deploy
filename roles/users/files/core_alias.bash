14d2abd1ff1e8e10886e390bdbfcd78882f9a6d0
Javier Rojas <fjrojas@aubay.es>
2022-07-15 21:20:38 +0200

    add new alias to follow a container logs

--- roles/users/files/core_alias.bash
+++ roles/users/files/core_alias.bash
@@ -4,4 +4,7 @@ alias ll='ls -alF'
 alias ls='ls --color=auto'
 alias sl="ls"
 alias sam_api_log_follow="docker logs --follow $(docker ps -f name=aubay-sam-stack_aubay-sam-api --quiet)"
+alias sam_iot_log_follow="docker logs --follow $(docker ps -f name=aubay-sam-stack_iot-gateway-api --quiet)"
+alias sam_web_log_follow="docker logs --follow $(docker ps -f name=aubay-sam-stack_aubay-sam-web --quiet)"
+alias sam_rabbitmq_log_follow="docker logs --follow $(docker ps -f name=aubay-sam-stack_rabbitmq --quiet)"
 #alias rabbitmqctl="docker exec -it $(docker ps -f name=aubay-sam-stack_rabbitmq --quiet) rabbitmqctl"
