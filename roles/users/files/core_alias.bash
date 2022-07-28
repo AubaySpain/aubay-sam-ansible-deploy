7b5f9214ba8e8bc2f4dc0cacd83faeed3a0c49ef
Javier Rojas <fjrojas@aubay.es>
2022-07-28 19:54:44 +0200

    add alias for keycloak logs

--- roles/users/files/core_alias.bash
+++ roles/users/files/core_alias.bash
@@ -3,10 +3,15 @@ alias l='ls -lCF'
 alias ll='ls -alF'
 alias ls='ls --color=auto'
 alias sl="ls"
+
+# stack logs
 alias sam_api_log_follow="docker logs --follow $(docker ps -f name=aubay-sam-stack_aubay-sam-api --quiet)"
 alias sam_iot_log_follow="docker logs --follow $(docker ps -f name=aubay-sam-stack_iot-gateway-api --quiet)"
 alias sam_web_log_follow="docker logs --follow $(docker ps -f name=aubay-sam-stack_aubay-sam-web --quiet)"
-alias sam_rabbitmq_log_follow="docker logs --follow $(docker ps -f name=aubay-sam-stack_rabbitmq --quiet)"
+alias rabbitmq_log_follow="docker logs --follow $(docker ps -f name=aubay-sam-stack_rabbitmq --quiet)"
+alias keycloak_log_follow="docker logs --follow $(docker ps -f name=aubay-sam-stack_keycloak --quiet)"
+
+# container's bash
 alias postgis_bash="docker exec -it $(docker ps -f name=aubay-sam-stack_postgis --quiet) bash"
 #alias rabbitmqctl="docker exec -it $(docker ps -f name=aubay-sam-stack_rabbitmq --quiet) rabbitmqctl"
 # docker exec -it CONTAINER_ID bash
